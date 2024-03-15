7f0d1162112ff0e6e711683efcf0c6ca7bbd1c81
pedroetec1930 <125662811+pedroetec1930@users.noreply.github.com>
2024-03-15 12:42:57 -0300

    Add files via upload

---
+++ projetos/conta/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    public class Conta
+    {
+        private string? Num_conta { get; set;  }
+        private double Saldo { get; set; }
+        private double Limite { get; set; }
+
+        public void consulta_saldo() 
+        {
+            Console.WriteLine("CONSULTANDO O SALDO");
+            Console.WriteLine("O seu saldo atual é de R$ " + Saldo);
+        }
+
+
+        public void depositar(double valor) 
+        {
+            Console.WriteLine("DEPÓSITO DE" + valor);
+            Saldo += valor;
+            
+
+        }
+
+
+        public void sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+            }
+
+            else
+            {
+                if (valor <= Limite)
+                {
+                    Console.WriteLine("Seu saldo é agora de" + Saldo);
+                }
+                else
+                {
+                    Console.WriteLine("LIMITE EXCEDIDO");
+                }
+            }
+        }
+
+
+        public void ajusta_limite(double valor)
+        {
+            Limite = valor;
+        }
+    }
+}
---
+++ projetos/conta/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Banco ETEC MCM!");
+
+            //Instanciar a classe Conta e criar um objeto dela
+
+            Conta conta = new Conta();
+
+            conta.depositar(1000);
+            conta.consulta_saldo();
+            conta.sacar(500);
+            conta.consulta_saldo();
+
+            conta.sacar(1000);
+            conta.consulta_saldo();
+
+            conta.sacar(10000);
+            conta.consulta_saldo();
+            
+
+        }
+    }
+}
