101e9eafc2f659d410c5e61d65e98b89d8c0ac94
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-09-02 08:59:30 -0300

    'restricao'

--- site_novo/painel/index.php
+++ site_novo/painel/index.php
@@ -1,6 +1,17 @@
 <?php
-    include 'conecta.php';
+    session_start();
+    if( (!isset($_SESSION['id']) == true) && 
+    (!isset($_SESSION['nome']) == true) && 
+    (!isset($_SESSION['email']) == true) ){
+        unset($_SESSION['id']);
+        unset($_SESSION['nome']);
+        unset($_SESSION['email']);
+        header('Location: ../index.html');
+    }
 
+
+
+    include 'conecta.php';
     include 'menu.php';
 ?>
 
